5816739f0bc130ed4509dc3936877d6d457818c4
Maru92 <marcopolo0595@hotmail.com>
2017-11-27 15:31:56 +0100

    op

--- GradSearchLGBMClassifier.py
+++ GradSearchLGBMClassifier.py
@@ -1,32 +1,16 @@
 import numpy as np 
 import pandas as pd 
+import json
+
 from sklearn.pipeline import Pipeline
 
 from lightgbm import LGBMClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
 
-from sklearn.metrics import make_scorer
 
 #%%
 train = pd.read_csv('../train.csv')
 test = pd.read_csv('../test.csv')
-
-#%% Gini
-def gini(actual, pred, cmpcol = 0, sortcol = 1):
-    all = np.asarray(np.c_[ actual, pred, np.arange(actual.shape[0]) ], dtype=np.float)
-    all = all[ np.lexsort((all[:,2], -1*all[:,1])) ]
-    totalLosses = all[:,0].sum()
-    giniSum = all[:,0].cumsum().sum() / totalLosses
- 
-    giniSum -= (actual.shape[0] + 1) / 2.
-    return giniSum / actual.shape[0]
-    
-def gini_normalized(a, p):
-    return gini(a, p) / gini(a, a)
-
-gini_score = make_scorer(gini_normalized,greater_is_better=True)
-gini_loss  = make_scorer(gini_normalized, greater_is_better=False)
 
 #%%
 
@@ -59,10 +43,6 @@ print(train.values.shape, test.values.shape)
 
 #%%
 
-X_train, X_test, y_train, y_test = train_test_split(train, target_train, test_size=0.2, random_state=42)
-
-#%%
-
 # LightGBM params
 lgb_params = {}
 lgb_params['learning_rate'] = 0.02
@@ -91,28 +71,37 @@ hyperparameters = { 'classifier__learning_rate': [0.02, 0.2],
                     'classifier__subsample': [0.7,0.9],
                     'classifier__subsample_freq': [1,10],
                     'classifier__colsample_bytree': [0.7,0.9],
-                    'classifier__silent': [False]
+                    'classifier__silent': [False],
+                    'classifier__seed': [200],
+                    'classifier__num_leaves': [16,31],
+                    'classifier__max_depth': [-1, 4, 10],
+                    'classifier__max_bin': [10, 255]
                   }
-clf = GridSearchCV(pipeline, hyperparameters, cv = 5, scoring = gini_score)
+#NB: When CV is an integer, it computes cv with stratifiedkfold, hence we don't need to split it before
+clf = GridSearchCV(pipeline, hyperparameters, cv = 6, scoring = 'roc_auc')
 
  
 # Fit and tune model
-clf.fit(X_train, y_train)
+clf.fit(train, target_train)
+
+print("Refiting")
 
 #refitting on entire training data using best settings
 clf.refit
 
-probs = clf.predict_proba(X_test)[:,1] 
+bestParam = clf.best_params_
 
-Gini_test = gini_normalized(y_test,probs)
-print("Refit, Gini normalized = ", Gini_test)
+dfg=open("bestParams.txt",'w')
+json.dump(bestParam,dfg)
+dfg.close()
+
+print(bestParam)
+
 
 
 #%%
 y_pred = clf.predict_proba(test)[:,1] 
 
-print(id_test.shape)
-print(y_pred.shape)
 
 sub_1 = pd.DataFrame()
 sub_1['id'] = id_test
@@ -120,5 +109,5 @@ sub_1['target'] = y_pred
 
 #%%
 
-sub_1.to_csv('GradSearchLGBM.csv', index = False)
+sub_1.to_csv('GridSearchCVLGBM.csv', index = False)
 
