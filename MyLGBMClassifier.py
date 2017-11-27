f3829528e7765e887471b53d8983075f90519bec
Maru92 <marcopolo0595@hotmail.com>
2017-11-27 13:31:56 +0100

    op

--- MyLGBMClassifier.py
+++ MyLGBMClassifier.py
@@ -7,27 +7,12 @@ from lightgbm import LGBMClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier
 
-from sklearn.metrics import make_scorer
 
 #%%
 train = pd.read_csv('../train.csv')
 test = pd.read_csv('../test.csv')
 
-#%% Gini
-def gini(actual, pred, cmpcol = 0, sortcol = 1):
-    all = np.asarray(np.c_[ actual, pred, np.arange(actual.shape[0]) ], dtype=np.float)
-    all = all[ np.lexsort((all[:,2], -1*all[:,1])) ]
-    totalLosses = all[:,0].sum()
-    giniSum = all[:,0].cumsum().sum() / totalLosses
- 
-    giniSum -= (actual.shape[0] + 1) / 2.
-    return giniSum / actual.shape[0]
-    
-def gini_normalized(a, p):
-    return gini(a, p) / gini(a, a)
 
-gini_score = make_scorer(gini_normalized,greater_is_better=True)
-gini_loss  = make_scorer(gini_normalized, greater_is_better=False)
 #%%
 
 id_test = test['id'].values
@@ -90,7 +75,7 @@ class Ensemble(object):
                 S_test_i[:, j] = clf.predict_proba(T)[:,1]
             S_test[:, i] = S_test_i.mean(axis=1)
 
-        results = cross_val_score(self.stacker, S_train, y, cv=3, scoring=gini_score)
+        results = cross_val_score(self.stacker, S_train, y, cv=3, scoring='roc_auc')
         print("Stacker score: %.5f" % (results.mean()))
 
         self.stacker.fit(S_train, y)
@@ -111,35 +96,30 @@ lgb_params['learning_rate'] = 0.02
 lgb_params['n_estimators'] = 650
 lgb_params['max_bin'] = 10
 lgb_params['subsample'] = 0.8
-lgb_params['subsample_freq'] = 10
-lgb_params['colsample_bytree'] = 0.8   
+lgb_params['subsample_freq'] = 10  
 lgb_params['min_child_samples'] = 500
 lgb_params['feature_fraction'] = 0.9
-lgb_params['num_iterations']=900
 lgb_params['bagging_freq'] = 1
-lgb_params['seed'] = 200
+lgb_params['seed'] = 500
 
 lgb_params2 = {}
 lgb_params2['n_estimators'] = 1090
-lgb_params2['learning_rate'] = 0.02
-lgb_params2['colsample_bytree'] = 0.3   
+lgb_params2['learning_rate'] = 0.02   
 lgb_params2['subsample'] = 0.7
 lgb_params2['subsample_freq'] = 2
 lgb_params2['num_leaves'] = 16
-lgb_params2['num_iterations']=900
-lgb_params2['feature_fraction'] = 0.9
+lgb_params2['feature_fraction'] = 0.8
 lgb_params2['bagging_freq'] = 1
-lgb_params2['seed'] = 200
+lgb_params2['seed'] = 500
 
 
 lgb_params3 = {}
 lgb_params3['n_estimators'] = 1100
 lgb_params3['max_depth'] = 4
-lgb_params3['num_iterations']=900
 lgb_params3['learning_rate'] = 0.02
-lgb_params3['feature_fraction'] = 0.9
+lgb_params3['feature_fraction'] = 0.95
 lgb_params3['bagging_freq'] = 1
-lgb_params3['seed'] = 200
+lgb_params3['seed'] = 500
 
 
 #lgb_params = {}
@@ -183,12 +163,15 @@ lgb_model3 = LGBMClassifier(**lgb_params3)
 
 
 #%%
-#log_model = LogisticRegression()
+log_model = LogisticRegression()
+#log_model = LGBMClassifier()
+
+##These methods seems to be costly and to slow the algorithm down
 #log_model = GradientBoostingClassifier()
 #log_model = RandomForestClassifier()
 #log_model = ExtraTreesClassifier()
 #log_model = AdaBoostClassifier()
-log_model = LGBMClassifier()
+
       
 stack = Ensemble(n_splits=10,
         stacker = log_model,
@@ -202,6 +185,18 @@ sub_1['id'] = id_test
 sub_1['target'] = y_pred
 
 #%%
+# Ensembling from different best submissions
 
-sub_1.to_csv('MyLGBM_gini_.csv', index = False)
+sub_2 = pd.read_csv('../Submissions/LGBMClassifier.csv')
+sub_3 = pd.read_csv('../Submissions/MyLGBM2.csv')
+sub_4 = pd.read_csv('../Submissions/MyLGBM1.csv')
+sub_5 = pd.read_csv('../Submissions/blend1.csv')
+sub_6 = pd.read_csv('../Submissions/DartLGBM.csv')
+
+sub = pd.DataFrame()
+sub['id'] = id_test
+sub['target'] = np.exp(np.mean([sub_1['target'].apply(lambda x: np.log(x)), sub_2['target'].apply(lambda x: np.log(x)), sub_3['target'].apply(lambda x: np.log(x)), sub_4['target'].apply(lambda x: np.log(x)), sub_5['target'].apply(lambda x: np.log(x)), sub_6['target'].apply(lambda x: np.log(x))], axis =0))
+
+sub.to_csv('sub.csv', index = False)
+
 
