08790293b9b225b0cb064305466e349b7e414f34
Maru92 <marc.jourdan@polytechnique.edu>
2017-12-03 19:19:17 +0100

    op

--- LGBM.py
+++ LGBM.py
@@ -2,8 +2,8 @@
 # Problems with casting rule : cannot safely convert passed user dtype of bool for int64 dtyped data
 # NB : doesn't use the user_logs information
 
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 import lightgbm as lgb
 from sklearn.model_selection import ShuffleSplit
 import gc
@@ -44,7 +44,7 @@ df_stats = grouped.agg({'msno' : {'total_order' : 'count'},
                          'actual_amount_paid' : {'mean_payment_each_transaction' : 'mean',
                                                   'total_actual_payment' : 'sum'},
                          'is_cancel' : {'cancel_times' : lambda x : sum(x==1)}})
-             
+
 df_stats.columns = df_stats.columns.droplevel(0)
 df_stats.reset_index(inplace=True)
 df_train = pd.merge(left = df_train,right = df_stats,how='left',on='msno')
@@ -52,17 +52,17 @@ df_train = pd.merge(left = df_train,right = df_stats,how='left',on='msno')
 del df_transactions,df_stats
 
 # The bd column have clear outlier, we clip it and reduce the type
-df_train['bd'].clip(0,100) 
+df_train['bd'].clip(0,100)
 df_train['bd'].fillna(0,inplace=True)
 df_train['bd'].astype(np.uint8,inplace=True)
 
 #%% Training phase
-bst = None 
+bst = None
 
-for train_indices,val_indices in ShuffleSplit(n_splits=1,test_size = 0.1,train_size=0.4).split(df_train): 
+for train_indices,val_indices in ShuffleSplit(n_splits=1,test_size = 0.1,train_size=0.4).split(df_train):
     train_data = lgb.Dataset(df_train.drop(['msno','is_churn'],axis=1).loc[train_indices,:],label=df_train.loc[train_indices,'is_churn'])
     val_data = lgb.Dataset(df_train.drop(['msno','is_churn'],axis=1).loc[val_indices,:],label=df_train.loc[val_indices,'is_churn'])
-    
+
     params = {
         'objective': 'binary',
         'metric': 'binary_logloss',
@@ -78,10 +78,10 @@ for train_indices,val_indices in ShuffleSplit(n_splits=1,test_size = 0.1,train_s
         'max_bin': 128,
         'max_depth': 10,
         'num_rounds': 200,
-        } 
-    
+        }
+
     bst = lgb.train(params, train_data, 100, valid_sets=[val_data])
-    
+
 #%% Import df_test with the same methodology
 df_test = pd.read_csv('../data/sample_submission_zero.csv',dtype = {'msno' : str})
 df_members = pd.read_csv('../data/members_v3.csv',dtype={'registered_via' : np.uint8})
@@ -117,7 +117,7 @@ df_stats = grouped.agg({'msno' : {'total_order' : 'count'},
                          'is_cancel' : {'cancel_times' : lambda x : sum(x==1)}})
 
 
-             
+
 df_stats.columns = df_stats.columns.droplevel(0)
 df_stats.reset_index(inplace=True)
 df_test = pd.merge(left = df_test,right = df_stats,how='left',on='msno')
@@ -129,5 +129,5 @@ df_test['bd'] = df_test['bd'].astype(np.uint8)
 
 predictions = bst.predict(df_test.drop(['msno','is_churn'],axis=1))
 df_test['is_churn'] = predictions
-df_test.drop(['city','bd','gender','registered_via','registration_init_time','expiration_date','total_order','plan_net_worth','mean_payment_each_transaction','total_actual_payment','cancel_times'],axis=1,inplace=True)
+df_test.drop(['city','bd','gender','registered_via','registration_init_time','total_order','plan_net_worth','mean_payment_each_transaction','total_actual_payment','cancel_times'],axis=1,inplace=True)
 df_test.to_csv('LGBM_0.csv',index=False)
