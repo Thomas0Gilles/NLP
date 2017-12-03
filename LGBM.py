3f5c7eaaeb93f3d66f14443372771151f87bc3b0
Maru92 <marc.jourdan@polytechnique.edu>
2017-12-03 16:04:06 +0100

    op

--- LGBM.py
+++ LGBM.py
@@ -1,4 +1,5 @@
 # Inspired by https://www.kaggle.com/talysacc/lgbm-starter-lb-0-23434
+# Problems with casting rule : cannot safely convert passed user dtype of bool for int64 dtyped data
 # NB : doesn't use the user_logs information
 
 import pandas as pd 
@@ -22,15 +23,13 @@ df_transactions = pd.read_csv('../data/transactions.csv',dtype = {'payment_metho
                                                                   'payment_plan_days' : np.uint8,
                                                                   'plan_list_price' : np.uint8,
                                                                   'actual_amount_paid': np.uint8,
-                                                                  'is_auto_renew' : np.bool,
-                                                                  'is_cancel' : np.bool})
+                                                                  'is_auto_renew' : np.bool}) #,is_cancel' : np.bool
 #NB: c'est pas dit que cela fonctionne étant donné que je prend une structure bien différente comparativement à l'ancien code
 df_transactions = pd.concat((df_transactions, pd.read_csv('../data/transactions_v2.csv', dtype = {'payment_method' : 'category',
                                                                   'payment_plan_days' : np.uint8,
                                                                   'plan_list_price' : np.uint8,
-                                                                  'actual_amount_paid': np.uint8,
-                                                                  'is_auto_renew' : np.bool,
-                                                                  'is_cancel' : np.bool})), axis=0, ignore_index=True).reset_index(drop=True)
+                                                                  'actual_amount_paid': np.uint8,  #,'is_cancel' : np.bool
+                                                                  'is_auto_renew' : np.bool})), axis=0, ignore_index=True).reset_index(drop=True)
 
 df_transactions = pd.merge(left = df_train,right = df_transactions,how='left',on='msno')
 grouped  = df_transactions.copy().groupby('msno')
